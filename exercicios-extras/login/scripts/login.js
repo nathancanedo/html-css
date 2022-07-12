374c4c3e92df8d6cd5da4e577ba123df130bb0a1
Nathan Canedo <101233593+nathancanedo@users.noreply.github.com>
2022-07-12 18:10:15 -0300

    Update login.js

--- exercicios-extras/login/scripts/login.js
+++ exercicios-extras/login/scripts/login.js
@@ -1,7 +1,39 @@
 function login() {
+
+    const users = [{user: 'nathan', password: 1234
+}, {user: 'caalf', password: 1234}]
+
+    users.push({user: 'jorge', password: 1234});
+
     impedirPaginaDeAtualizar()
 
     function impedirPaginaDeAtualizar(){
         const form = document.querySelector('#form');
         form.addEventListener('submit', e => {e.preventDefault()});
     }
+
+    verificarLogin();
+
+    function verificarLogin() {
+        const user = document.querySelector('#user').value;
+        const password = document.querySelector('#password').value;
+
+        const usuarioLiberado = usuarioESenha(user, password);
+
+        function usuarioESenha(user, password) {
+            for (let c in users) {
+                if (users[c].user === user && users[c].password == password) {
+                    console.log('acesso liberado');
+                    return true;
+                } else {
+                    console.log(`acesso negado ${users[c].user}`);
+                }
+            }
+        }
+        if (usuarioLiberado) {
+            alert('Bem Vindo!')
+        } else {
+            alert('usuario ou senha incorretos')
+        }
+    }  
+}
